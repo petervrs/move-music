e5ae1e7fdd907f774468c2a5fbcd7a4b2c04371b
Peter Verschuure <peterverschuure@hotmail.com>
2018-01-04 12:27:38 +0100

    Finish and cleanup script

--- move.py
+++ move.py
@@ -1,56 +1,67 @@
-## Main goal:
-# Everything should be placed in one big directory
-
-# TODO: Check what's up with the album covers
-# TODO: Check how empty subdirectories are handled
-# TODO: Report amount of files moved
-# TODO: Print each file which will be moved
-
+## This script moves all files from subdirectories in the directory in which it is run to
+# this directory
+# TODO: Make sure the amount of moved files is correctly calculated
 
 import os
 import shutil
 
+amountOfFilesMoved = 0
 level0SubDirectories = next(os.walk('.'))[1]
 level0Files = next(os.walk("."))[2]
 mainDirectory = os.getcwd()
 
 print("Currently working in directory " + mainDirectory)
-print("The following subdirectories are present: ", level0SubDirectories)
-print("The following files are present: ", level0Files)
-print("Starting to move files to main directory...")
+print("The following subdirectories are present: ")
+print(level0SubDirectories)
+print("The following files are present: ")
+print(level0Files)
+print("Starting to move files to main directory...\n")
 
 currentWorkingDirectory = os.getcwd()
 currentDirLevel = 0
 
-## Make a recursive function
 def moveFilesToMainDirectory(path):
     subDirs = next(os.walk(path))[1]
     files = next(os.walk(path))[2]
-    print("Subdirectories in ", path, ": ", subDirs)
-    print("Files in ", path, ": ", files)
+    print("Subdirectories in " + path + ": ")
+    print(subDirs)
+    print("Files in " + path + ": ")
+    print(files)
 
     if len(subDirs) > 0:
-        print("There are subdirectories")
+        print("There are subdirectories present")
         # For each subdirectory, call this function
         for subDir in subDirs:
+            print("Moving files in subdirectories")
             moveFilesToMainDirectory(os.path.join(path, subDir))
+        
     else:
-        print("There are no subdirectories")
-        # Check if there are files present
-        if len(files) > 0:
-            # Move files to main directory
-            for file in files:
-                currentFilePath = os.path.join(path, file)
-                newFilePath = os.path.join(mainDirectory, file)
-                shutil.move(currentFilePath, newFilePath)
-        else:
-            # Step one directory back and remove directory
-            os.chdir("../")
-            print("Current directory: ", os.getcwd())
-            # NOTE: we can assume we have no files in this directory I guess?
-            os.rmdir(path)
+        print("There are no subdirectories present")
+
+    # Check if there are files present
+    if len(files) > 0:
+        # If there are files present, move each file to main directory
+        print("There are files present, moving files to main directory")
+        # Move files to main directory
+        for file in files:
+            currentFilePath = os.path.join(path, file)
+            newFilePath = os.path.join(mainDirectory, file)
+            print("Moving file " + currentFilePath + " to " + newFilePath)
+            shutil.move(currentFilePath, newFilePath)
+            global amountOfFilesMoved
+            amountOfFilesMoved += 1
+    else:
+        print("There are no files present")
+
+    # Step one directory back and remove directory
+    os.chdir("../")
+    print("Current directory: " + os.getcwd())
+    print("Removing directory: " + path)
+    os.rmdir(path)
     return
 
-moveFilesToMainDirectory(os.path.join(currentWorkingDirectory, level0SubDirectories[0]))
 
-# Call testFunction for each subdirectory of the main directory
+for directory in level0SubDirectories:
+    moveFilesToMainDirectory(os.path.join(currentWorkingDirectory, directory))
+
+print("\nFiles moved to main directory, " + str(amountOfFilesMoved) + " files were moved")
